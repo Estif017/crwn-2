a9f5610fe25332ed2cbda8470f3c48852f0e870a
Estif017 <estifoteklearegay@gmail.com>
2020-11-23 16:46:14 -0800

    stored user data to firestore and to the app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
-import { auth } from './firebase/firebase-util';
+import { auth, createUserProfileDocument } from './firebase/firebase-util';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/shopPage/ShopPage';
 import SignInSignUpPage from './pages/sign-in-sign-up/SignInSignUpPage';
@@ -11,12 +11,25 @@ function App() {
   const [currentUser,setCurrentUser]=useState(null)
   let unSubscribeFromAuth=null
   useEffect(()=>{
-      unSubscribeFromAuth=auth.onAuthStateChanged(user=>{
-        setCurrentUser(user)
-        console.log(user);
+    // eslint-disable-next-line
+      unSubscribeFromAuth=auth.onAuthStateChanged(async userAuth=>{
+        if(userAuth){
+          const userRef=await createUserProfileDocument(userAuth)
+
+          userRef.onSnapshot(snapshot=>{
+            setCurrentUser({
+              id:snapshot.id,
+              ...snapshot.data()
+            })
+          })
+        }else{
+          setCurrentUser(userAuth)
+        }
       })
       return ()=> unSubscribeFromAuth()
+      
     },[])
+    console.log(currentUser);
   return (
     <div >
       <Header currentUser={currentUser}/>
--- src/firebase/firebase-util.js
+++ src/firebase/firebase-util.js
@@ -12,12 +12,38 @@ var config = {
     appId: "1:1063901345099:web:780b46b04f261de5d2e4ce",
     measurementId: "G-WFMECZY04C"
   };
+
+  export const createUserProfileDocument= async(userAuth,aditionalData)=>{
+    if(!userAuth) return
+
+    const userRef= firestore.doc(`users/${userAuth.uid}`)
+    const snapShot= await userRef.get()
+
+    if(!snapShot.exists){
+      const {displayName,email}=userAuth;
+      const createdAt = new Date()
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...aditionalData
+        })
+      } catch (error) {
+       console.log('error occur creating user', error.message); 
+      }
+    }
+    return userRef
+  }
+
   // Initialize Firebase
   firebase.initializeApp(config);
 
   export const auth = firebase.auth()
   export const firestore = firebase.firestore()
 
+  //sign in with google
   const provider = new firebase.auth.GoogleAuthProvider()
   provider.setCustomParameters({ prompt:'select_account' })
   export const SignInWithGoogle=()=>auth.signInWithPopup(provider)
